35d46c7fbb65e1a6986acf49e4235f5d49efb4e9
Abhishek kumar <abhishek.kumar@avalara.com>
2022-09-13 17:58:26 +0530

    Add project files.

---
+++ EFCodeFirstApplication/Migrations/202208301134510_AddCategoryTable.cs
@@ -0,0 +1,34 @@
+﻿namespace EFCodeFirstApplication.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Courses", "Category_Id", c => c.Int());
+            CreateIndex("dbo.Courses", "Category_Id");
+            AddForeignKey("dbo.Courses", "Category_Id", "dbo.Categories", "Id");
+            Sql("INSERT INTO CATEGORIES VALUES('Web Development')");
+            Sql("INSERT INTO CATEGORIES VALUES('Web Programming')");
+        }
+
+        public override void Down()
+        {
+            DropForeignKey("dbo.Courses", "Category_Id", "dbo.Categories");
+            DropIndex("dbo.Courses", new[] { "Category_Id" });
+            DropColumn("dbo.Courses", "Category_Id");
+            DropTable("dbo.Categories");
+        }
+    }
+}
---
+++ EFCodeFirstApplication/Migrations/202208310852597_AddTempTable.cs
@@ -0,0 +1,31 @@
+﻿namespace EFCodeFirstApplication.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddTempTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Temps",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            AddColumn("dbo.Categories", "Temp_ID", c => c.Int());
+            CreateIndex("dbo.Categories", "Temp_ID");
+            AddForeignKey("dbo.Categories", "Temp_ID", "dbo.Temps", "ID");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Categories", "Temp_ID", "dbo.Temps");
+            DropIndex("dbo.Categories", new[] { "Temp_ID" });
+            DropColumn("dbo.Categories", "Temp_ID");
+            DropTable("dbo.Temps");
+        }
+    }
+}
