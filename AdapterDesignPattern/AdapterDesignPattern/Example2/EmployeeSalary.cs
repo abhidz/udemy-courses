5e33be66d1b767ecf3742f5900d436abe6f589e0
Abhishek kumar <abhishek.kumar@avalara.com>
2023-07-10 18:38:17 +0530

    EmployeeSalary Example

---
+++ AdapterDesignPattern/AdapterDesignPattern/Example2/EmployeeSalary.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace AdapterDesignPattern.Example2
+{
+    public class EmployeeSalary
+    {
+        public EmployeeSalary(string name, double salary)
+        {
+            Name = name;
+            this.Salary = salary;
+        }
+
+        public string Name { get; set; }
+
+        public double Salary { get; set; }
+    }
+
+    public class ThirdPartyBillingSystem
+    {
+        public void ProcessSalary(List<EmployeeSalary> salaryList)
+        {
+            foreach (var item in salaryList)
+            {
+                Console.WriteLine($"{item.Salary} is credited to {item.Name}");
+            }
+        }
+    }
+
+    public interface ITarget
+    {
+        void ProcessCompanySalary(string[,] employeesArray);
+    }
+
+    public class EmployeeAdapter : ITarget
+    {
+        private readonly ThirdPartyBillingSystem billingSystem = new ThirdPartyBillingSystem();
+
+        public void ProcessCompanySalary(string[,] employeesArray)
+        {
+            string Name = null;
+            string Salary = null;
+            List<EmployeeSalary> listEmployee = new List<EmployeeSalary>();
+            for (int i = 0; i < employeesArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < employeesArray.GetLength(1); j++)
+                {
+                    if (j == 0)
+                    {
+                        Name = employeesArray[i, j];
+                    }
+                    else if (j == 1)
+                    {
+                        Salary = employeesArray[i, j];
+                    }
+                }
+                listEmployee.Add(new EmployeeSalary(Name, Convert.ToDouble(Salary)));
+            }
+             billingSystem.ProcessSalary(listEmployee);
+        }
+    }
+
+}
--- AdapterDesignPattern/AdapterDesignPattern/Program.cs
+++ AdapterDesignPattern/AdapterDesignPattern/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdapterDesignPattern.Example2;
+using System;
 
 namespace AdapterDesignPattern
 {
@@ -6,15 +7,24 @@ namespace AdapterDesignPattern
     {
         static void Main(string[] args)
         {
-            IBird bird = new Sparrow();
-            bird.fly();
-            bird.makeSound();
+            //IBird bird = new Sparrow();
+            //bird.fly();
+            //bird.makeSound();
 
-            IToyDuck toyDuck = new PlasticToyDuck();
-            toyDuck.squeak();
+            //IToyDuck toyDuck = new PlasticToyDuck();
+            //toyDuck.squeak();
 
-            BirdAdaptor birdAdaptor = new BirdAdaptor(bird);
-            birdAdaptor.squeak();
+            //BirdAdaptor birdAdaptor = new BirdAdaptor(bird);
+            //birdAdaptor.squeak();
+
+            // EmployeeSalary
+            ITarget target = new EmployeeAdapter();
+            string[,] empArray = new string[,]
+            {
+               { "Abhishek", "10000" },
+               { "Riya", "1000000" }
+            };
+            target.ProcessCompanySalary(empArray);
             Console.ReadLine();
         }
     }
