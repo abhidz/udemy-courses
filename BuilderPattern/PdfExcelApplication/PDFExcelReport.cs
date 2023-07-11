2fa7890b57b6303946b31b8c869060dd497298ef
Abhishek kumar <abhishek.kumar@avalara.com>
2023-07-11 17:15:49 +0530

    PDFExcelReport

---
+++ BuilderPattern/PdfExcelApplication/PDFExcelReport.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderPattern.PdfExcelApplication
+{
+    public class PdfExcelReport
+    {
+        public string Header { get; set; }
+        public string Footer { get; set; }
+
+    }
+
+    public abstract class ReportBuilder
+    {
+        public abstract void setHeader(PdfExcelReport report);
+        public abstract void setFooter(PdfExcelReport report);
+    }
+
+    public class PdfReport : ReportBuilder
+    {
+        public override void setFooter(PdfExcelReport report)
+        {
+            report.Header = " PDF Header";
+        }
+
+        public override void setHeader(PdfExcelReport report)
+        {
+            report.Footer = " PDF Footer";
+        }
+    }
+
+    public class ExcelReport : ReportBuilder
+    {
+        public override void setFooter(PdfExcelReport report)
+        {
+            report.Header = " Excel Header";
+        }
+
+        public override void setHeader(PdfExcelReport report)
+        {
+            report.Footer = " Excel Footer";
+        }
+    }
+
+    public class ReportDirector
+    {
+        public void DisplayReport(ReportBuilder report)
+        {
+            var obj = new PdfExcelReport();
+            report.setHeader(obj);
+            report.setFooter(obj);
+            Console.WriteLine(obj.Header + " " + obj.Footer);
+        }
+
+    }
+
+}
--- BuilderPattern/Program.cs
+++ BuilderPattern/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BuilderPattern.PdfExcelApplication;
+using System;
 
 namespace BuilderPattern
 {
@@ -6,18 +7,24 @@ namespace BuilderPattern
     {
         static void Main(string[] args)
         {
-            var scooterDirector = new VehicleDirector(new Scooter());
-            scooterDirector.CreateObjects();
-            var scooterSpecifications = scooterDirector.GetVehicle();
-            Console.WriteLine("Scooter EngineNumber =" + scooterSpecifications.EngineNumber);
-            Console.WriteLine("Scooter Model =" + scooterSpecifications.Model);
+            //var scooterDirector = new VehicleDirector(new Scooter());
+            //scooterDirector.CreateObjects();
+            //var scooterSpecifications = scooterDirector.GetVehicle();
+            //Console.WriteLine("Scooter EngineNumber =" + scooterSpecifications.EngineNumber);
+            //Console.WriteLine("Scooter Model =" + scooterSpecifications.Model);
 
 
-            var carDirector = new VehicleDirector(new Car());
-            carDirector.CreateObjects();
-            var carSpecifications = carDirector.GetVehicle();
-            Console.WriteLine("Car EngineNumber =" + carSpecifications.EngineNumber);
-            Console.WriteLine("Car Model =" + carSpecifications.Model);
+            //var carDirector = new VehicleDirector(new Car());
+            //carDirector.CreateObjects();
+            //var carSpecifications = carDirector.GetVehicle();
+            //Console.WriteLine("Car EngineNumber =" + carSpecifications.EngineNumber);
+            //Console.WriteLine("Car Model =" + carSpecifications.Model);
+
+            // PdfExcelReport
+            ReportDirector report = new ReportDirector();
+            report.DisplayReport(new PdfReport());
+            report.DisplayReport(new ExcelReport());
+            Console.ReadLine();
         }
     }
 }
