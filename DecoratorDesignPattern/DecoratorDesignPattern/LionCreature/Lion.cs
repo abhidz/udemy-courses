633cde20a9a7e95b73339335873b76d7be6ac6d0
Abhishek kumar <abhishek.kumar@avalara.com>
2023-07-12 17:19:18 +0530

    LionDecorator

---
+++ DecoratorDesignPattern/DecoratorDesignPattern/LionCreature/Lion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorDesignPattern.LionCreature
+{
+    public interface ILion
+    {
+        string Roar();
+    }
+
+    public class Lion : ILion
+    {
+        public string Roar()
+        {
+            return "ROAR";
+        }
+    }
+
+    public class LoudLionDecorator : ILion
+    {
+        private readonly ILion _lion;
+
+        public LoudLionDecorator(ILion lion)
+        {
+            this._lion = lion;
+        }
+        public string Roar()
+        {
+           return this._lion.Roar() + "loudly";
+        }
+    }
+
+    public class LouderLionDecorator : ILion
+    {
+        private readonly ILion _lion;
+
+        public LouderLionDecorator(ILion lion)
+        {
+            this._lion = lion;
+        }
+        public string Roar()
+        {
+            return this._lion.Roar() + "loudlier";
+        }
+    }
+
+    public class SilentLionDecorator : ILion
+    {
+        private readonly ILion _lion;
+
+        public SilentLionDecorator(ILion lion)
+        {
+            this._lion = lion;
+        }
+        public string Roar()
+        {
+            return "Meow";
+        }
+    }
+}
--- DecoratorDesignPattern/DecoratorDesignPattern/Program.cs
+++ DecoratorDesignPattern/DecoratorDesignPattern/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DecoratorDesignPattern.LionCreature;
+using System;
 
 namespace DecoratorDesignPattern
 {
@@ -6,26 +7,40 @@ namespace DecoratorDesignPattern
     {
         static void Main(string[] args)
         {
-            Book book = new Book("Worley", "Inside ASP.NET", 10);
-            book.Display();
-            // Create video
-            Video video = new Video("Spielberg", "Jaws", 23, 92);
-            video.Display();
-            LibraryItem bookItem = new Book("ahas", "hags", 2);
-           
-            // Make video borrowable, then borrow and display
-            Console.WriteLine("\nMaking video borrowable:");
-            Borowable borrowvideo = new Borowable(video);
-            borrowvideo.BorrowItem("Customer #1");
-            borrowvideo.BorrowItem("Customer #2");
-            borrowvideo.Display();
+            //Book book = new Book("Worley", "Inside ASP.NET", 10);
+            //book.Display();
+            //// Create video
+            //Video video = new Video("Spielberg", "Jaws", 23, 92);
+            //video.Display();
+            //LibraryItem bookItem = new Book("ahas", "hags", 2);
 
-            // Make book borrowable, then borrow and display
-            Console.WriteLine("\nMaking book borrowable:");
-            Borowable borrowbook = new Borowable(book);
-            borrowvideo.BorrowItem("Customer #3");
-            borrowvideo.BorrowItem("Customer #4");
-            borrowvideo.Display();
+            //// Make video borrowable, then borrow and display
+            //Console.WriteLine("\nMaking video borrowable:");
+            //Borowable borrowvideo = new Borowable(video);
+            //borrowvideo.BorrowItem("Customer #1");
+            //borrowvideo.BorrowItem("Customer #2");
+            //borrowvideo.Display();
+
+            //// Make book borrowable, then borrow and display
+            //Console.WriteLine("\nMaking book borrowable:");
+            //Borowable borrowbook = new Borowable(book);
+            //borrowvideo.BorrowItem("Customer #3");
+            //borrowvideo.BorrowItem("Customer #4");
+            //borrowvideo.Display();
+
+            // Lion Decorator
+            Lion lion = new Lion();
+            Console.WriteLine(lion.Roar());
+
+            LoudLionDecorator loudLionDecorator = new LoudLionDecorator(lion);
+            Console.WriteLine(loudLionDecorator.Roar());
+
+            LouderLionDecorator louderLionDecorator = new LouderLionDecorator(lion);
+            Console.WriteLine(louderLionDecorator.Roar());
+
+            SilentLionDecorator silentLionDecorator = new SilentLionDecorator(lion);
+            Console.WriteLine(silentLionDecorator.Roar());
+
             Console.ReadLine();
         }
     }
